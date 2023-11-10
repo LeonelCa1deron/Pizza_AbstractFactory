1a4009c5177c8e3e0bae9bb789e783cc05088844
Leonel Calderón <112144139+LeonelCa1deron@users.noreply.github.com>
2023-11-10 00:07:28 -0800

    first commit

---
+++ ChicagoPizzaIngredientFactory.cs
@@ -0,0 +1,33 @@
+public class ChicagoPizzaIngredientFactory : PizzaIngredientFactory
+{
+    public Dough CreateDough()
+    {
+        return new ThickCrustDough();
+    }
+
+    public Sauce CreateSauce()
+    {
+        return new PlumTomatoSauce();
+    }
+
+    public Cheese CreateCheese()
+    {
+        return new MozzarellaCheese();
+    }
+
+    public Veggies[] CreateVeggies()
+    {
+        Veggies[] veggies = { new BlackOlives(), new Spinach(), new Eggplant() };
+        return veggies;
+    }
+
+    public Pepperoni CreatePepperoni()
+    {
+        return new SlicedPepperoni();
+    }
+
+    public Clams CreateClam()
+    {
+        return new FrozenClams();
+    }
+}
